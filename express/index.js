18acaf8054ef9e0f5e0a062a5f5530d1b7df4abd
Raymond Feng <raymond@strongloop.com>
2015-01-13 16:00:21 -0800

    Add cors/bodyParser middleware for functional parity

--- express/index.js
+++ express/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
 var compression = require('compression');
+var cors = require('cors');
+var bodyParser = require('body-parser');
 var favicon = require('serve-favicon');
 var path = require('path');
 
 app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use(cors());
 app.use(compression());
+app.use(bodyParser.json({strict: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/api/messages/greet', function(req, res) {
   res.json({greet: 'hello world'});
